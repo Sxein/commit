cb38317ff7f2d4b9e65424b38ff2edf064f16650
Zayar Shein <alexleotwd@gmail.com>
2026-05-02 13:37:22 +0800

    feat: add delete and update functionality for commits in commitController and routes

--- server/src/controllers/commitController.ts
+++ server/src/controllers/commitController.ts
@@ -11,7 +11,7 @@ export const createCommit = async (req: AuthRequest, res: Response) => {
     }
 
     try {
-        const { title} = req.body;
+        const { title } = req.body;
         const newCommit = await prisma.commit.create({
             data: {
                 title,
@@ -43,5 +43,61 @@ export const getAllCommits = async (req: AuthRequest, res: Response) => {
     } catch (error) {
         console.error('Error fetching commits:', error);
         res.status(500).json({ error: 'An error occurred while fetching the commits.' });
+    }
+}
+
+export const deleteCommit = async (req: AuthRequest, res: Response) => {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+        return res.status(500).json({ error: 'User information is missing in the request.' });
+    }
+
+    try {
+        const { commitId } = req.params;
+        const deletedCommit = await prisma.commit.deleteMany({
+            where: {
+                id: Number(commitId),
+                userId: userId
+            }
+        })
+        if (deletedCommit.count === 0) {
+            return res.status(404).json({ error: 'Commit not found or you are not the owner.' });
+        }
+        res.status(200).json({ message: 'Commit deleted successfully.', deletedCount: deletedCommit.count });
+    }
+    catch (error) {
+        console.error('Error deleting commit:', error);
+        res.status(500).json({ error: 'An error occurred while deleting the commit.' });
+    }
+}
+
+export const updateCommit = async (req: AuthRequest, res: Response) => {
+    const userId = req.user?.userId;
+
+    if (!userId) {
+        return res.status(500).json({ error: 'User information is missing in the request.' });
+    }
+
+    try {
+        const { commitId } = req.params;
+        const { title } = req.body;
+
+        const updatedCommit = await prisma.commit.updateMany({
+            where: {
+                id: Number(commitId),
+                userId: userId
+            },
+            data: {
+                title
+            }
+        })
+        if (updatedCommit.count === 0) {
+            return res.status(404).json({ error: 'Commit not found or you are not the owner.' });
+        }
+        res.status(200).json({ message: 'Commit updated successfully.', updatedCount: updatedCommit.count });
+    } catch (error) {
+        console.error('Error updating commit:', error);
+        res.status(500).json({ error: 'An error occurred while updating the commit.' });
     }
 }
--- server/src/routes/commitRoutes.ts
+++ server/src/routes/commitRoutes.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
-import { createCommit, getAllCommits } from '../controllers/commitController.js';
+import { createCommit, getAllCommits, deleteCommit, updateCommit } from '../controllers/commitController.js';
 import { authenticate } from '../middleware/authMiddleware.js';
 
 const commitRoutes = Router();
 
 commitRoutes.post('/', authenticate, createCommit);
+commitRoutes.put('/:commitId', authenticate, updateCommit);
+commitRoutes.delete('/:commitId', authenticate, deleteCommit);
 commitRoutes.get('/', authenticate, getAllCommits);
 
 export default commitRoutes;
