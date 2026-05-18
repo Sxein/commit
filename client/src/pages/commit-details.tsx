92ef9721547851985d70f03c9c28692044842f53
Zayar Shein <alexleotwd@gmail.com>
2026-05-18 08:55:26 +0800

    feat: integrate activity calendar and remove Heatmap component

--- client/src/pages/commit-details.tsx
+++ client/src/pages/commit-details.tsx
@@ -1,7 +1,10 @@
-import Heatmap  from "@/components/Heatmap"
 import { useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query";
 import { fetchCommitLogs } from "@/services/api";
+import { Loader2 } from "lucide-react";
+import {ActivityCalendar, type ThemeInput} from "react-activity-calendar";
+import { subDays, format } from "date-fns";
+import type { CommitLog } from "@/types";
 
 export default function CommitDetails() {
     const { id } = useParams();
@@ -12,11 +15,47 @@ export default function CommitDetails() {
         queryFn: () => fetchCommitLogs(commitId)
     })
 
+    const completedDateSet = new Set(logs?.map((log: CommitLog) => log.date.split('T')[0]));
+
+    const calendarData = Array.from({ length: 365 }, (_, i) => {
+        const date = subDays(new Date(), 364 - i);
+        const dateStr = format(date, 'yyyy-MM-dd');
+
+        const isCompleted = completedDateSet.has(dateStr);
+
+        return {
+            date: dateStr,
+            count: isCompleted ? 1 : 0,
+            level: isCompleted ? 1 : 0
+        };
+    })
+
+    const twoColorTheme: ThemeInput = {
+        light: ['#ebedf0', '#FF9B00'],
+}
 
     return (
-        <div className="p-6">
-            <Heatmap
-            logs={logs} />
-        </div>
+        <>
+            <p>Commit Details</p>
+            <div className="flex justify-center items-center h-64">
+                <ActivityCalendar
+                data={calendarData}
+                minLevel={0}
+                maxLevel={1}
+                showWeekdayLabels={true}
+                theme= {twoColorTheme}
+                blockMargin = {5}
+                blockSize = {20}
+                blockRadius={4}
+                showColorLegend={false}
+                showTotalCount={false}
+                tooltips={{
+                    activity: {
+                        text: activity => `${activity.date}`
+                    },
+                }}
+            />
+            </div>
+        </>
     )
 }
